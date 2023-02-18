73cd49ac57a30a23662d8a32c4300a13490225ef
Tiago <tiagohenriqueodoia@gmail.com>
2023-02-18 12:04:55 -0300

    Add files via upload

--- nvim/lua/lsp/null-ls.lua
+++ nvim/lua/lsp/null-ls.lua
@@ -1,23 +1,26 @@
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local completion = null_ls.builtins.completion
 
 local sources = {
 	formatting.rubocop,
-	formatting.eslint,
-	formatting.autopep8,
-	formatting.stylua,
-	formatting.clang_format,
-	-- formatting.prettier,
-	-- formatting.latexindent,
+	diagnostics.eslint,
+	completion.spell,
+	--formatting.eslint,
+	--formatting.autopep8,
+	--formatting.stylua,
+	--formatting.clang_format,
+	--formatting.prettier,
+	--formatting.latexindent,
 }
 
 null_ls.setup({
 	sources = sources,
-
 	on_attach = function(client)
 		if client.server_capabilities.documentFormattingProvider then
-			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format { asyc = true }")
+			--vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format { asyc = true }")
 		end
 	end,
 })
